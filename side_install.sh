137f6349b223891eb5f6e983c75068f1feccfd97
KrakeNocto <octo2kraken@gmail.com>
2024-10-08 19:25:16 +0300

    Update side_install.sh

--- side_install.sh
+++ side_install.sh
@@ -35,7 +35,7 @@ Restart=on-failure
 RestartSec=10
 LimitNOFILE=65535
 Environment="DAEMON_HOME=$HOME/.side"
-Environment="DAEMON_NAME=sided"
+Environment="DAEMON_NAME=side"
 Environment="UNSAFE_SKIP_BACKUP=true"
 Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.side/cosmovisor/current/bin"
 
