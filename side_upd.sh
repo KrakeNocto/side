ca8e1d6f742a7431703dbeef2d79a2ab87218cdc
KrakeNocto <octo2kraken@gmail.com>
2024-10-09 14:52:59 +0300

    Update side_upd.sh

--- side_upd.sh
+++ side_upd.sh
@@ -6,9 +6,12 @@ chmod +x /root/.side/cosmovisor/genesis/bin/sided
 sed -i 's|ExecStart=/root/go/bin/cosmovisor run start --home /root/.side|ExecStart=/root/go/bin/cosmovisor start --home /root/.side|' /etc/systemd/system/sided.service
 sed -i 's|Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=true"|Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"|' /etc/systemd/system/sided.service
 
+cp $HOME/.side/data/priv_validator_state.json $HOME/.side/priv_validator_state.json.backup
+rm -rf $HOME/.side/data
 curl -L https://snapshots.kjnodes.com/side-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.side
 [[ -f $HOME/.side/data/upgrade-info.json ]] && cp $HOME/.side/data/upgrade-info.json $HOME/.side/cosmovisor/genesis/upgrade-info.json
+mv $HOME/.side/priv_validator_state.json.backup $HOME/.side/data/priv_validator_state.json
 
 rm side_upd.sh
 
-systemctl restart sided && journalctl -fu sided
+systemctl daemon-reload && systemctl restart sided && journalctl -fu sided
