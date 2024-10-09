262e0b4f8a335826e5ce5793cd5d81057f5cf7f7
KrakeNocto <octo2kraken@gmail.com>
2024-10-09 14:45:21 +0300

    Update side_upd.sh

--- side_upd.sh
+++ side_upd.sh
@@ -3,6 +3,9 @@ systemctl stop sided
 wget http://162.55.92.13:11558/sided && mv sided /root/.side/cosmovisor/genesis/bin/
 chmod +x /root/.side/cosmovisor/genesis/bin/sided
 
+sed -i 's|ExecStart=/root/go/bin/cosmovisor run start --home /root/.side|ExecStart=/root/go/bin/cosmovisor start --home /root/.side|' /etc/systemd/system/sided.service
+sed -i 's|Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=true"|Environment="DAEMON_ALLOW_DOWNLOAD_BINARIES=false"|' /etc/systemd/system/sided.service
+
 curl -L https://snapshots.kjnodes.com/side-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.side
 [[ -f $HOME/.side/data/upgrade-info.json ]] && cp $HOME/.side/data/upgrade-info.json $HOME/.side/cosmovisor/genesis/upgrade-info.json
 
