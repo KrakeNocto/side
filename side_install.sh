31839497e2eb99fd60cc457cccacad45766ffb0d
KrakeNocto <octo2kraken@gmail.com>
2024-10-14 17:20:33 +0300

    Update side_install.sh

--- side_install.sh
+++ side_install.sh
@@ -2,6 +2,17 @@ sudo apt -q update
 sudo apt -qy install curl git jq lz4 libwasmvm-dev build-essential
 sudo apt -qy upgrade
 
+cd $HOME
+VER="1.21.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
 echo "Moniker:"
 read -r MONIKER
 
