a9a3d097101334dac682ca5b7e3f9882a20fe339
KrakeNocto <octo2kraken@gmail.com>
2024-10-21 17:25:58 +0300

    Update create_validator.sh

--- create_validator.sh
+++ create_validator.sh
@@ -10,9 +10,9 @@ read -r MONIKER
 echo $WALLET_1_MNEM | /root/go/bin/sided keys add wallet_1 --keyring-backend test --key-type="taproot" --recover --hd-path="m/86'/1'/0'/0/0"
 echo $WALLET_2_MNEM | /root/go/bin/sided keys add wallet_2 --recover --keyring-backend test
 
-echo "Sleeping 43200 seconds (12 hours)"
+echo "Sleeping 86400 seconds (24 hours)"
 
-sleep 43200
+sleep 86400
 
 min_time_s=1200
 max_time_s=14400
