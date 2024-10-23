285f7c7c1bda1dfeb5265a8e94022d3f94f0c704
KrakeNocto <octo2kraken@gmail.com>
2024-10-23 14:22:11 +0300

    Update side_upd_it.sh

--- side_upd_it.sh
+++ side_upd_it.sh
@@ -1,6 +1,6 @@
 systemctl stop side
 
-wget http://148.251.46.18:11558/sided && mv sided /root/go/bin/
+wget http://162.55.92.13:12312/sided && mv sided /root/go/bin/
 chmod +x /root/go/bin/sided
 
 curl -Ls https://snapshots.kjnodes.com/side-testnet/addrbook.json > $HOME/.side/config/addrbook.json
@@ -8,7 +8,7 @@ curl -Ls https://snapshots.kjnodes.com/side-testnet/addrbook.json > $HOME/.side/
 cp $HOME/.side/data/priv_validator_state.json $HOME/.side/priv_validator_state.json.backup
 rm -rf $HOME/.side/data
 rm -rf $HOME/.side/data $HOME/.side/wasm
-curl https://server-5.itrocket.net/testnet/side/side_2024-10-18_255075_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.side
+curl https://server-5.itrocket.net/testnet/side/side_2024-10-23_327970_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.side
 mv $HOME/.side/priv_validator_state.json.backup $HOME/.side/data/priv_validator_state.json
 
 rm side_upd_it.sh
