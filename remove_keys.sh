c89d2f82ca20a1c42fa09f41cf1dcb756c842c5c
KrakeNocto <octo2kraken@gmail.com>
2024-10-23 16:59:11 +0300

    Update remove_keys.sh

--- remove_keys.sh
+++ remove_keys.sh
@@ -3,12 +3,12 @@
 KEYRING_BACKEND="test"
 
 # Получение списка ключей
-KEYS=$(/root/.side/cosmovisor/genesis/bin/sided keys list --keyring-backend $KEYRING_BACKEND --output json | jq -r '.[].name')
+KEYS=$(/root/.side/cosmovisor/upgrades/v0.9.3/bin/sided keys list --keyring-backend $KEYRING_BACKEND --output json | jq -r '.[].name')
 
 # Удаление каждого ключа
 for KEY in $KEYS; do
   echo "Удаляем ключ: $KEY"
-  /root/.side/cosmovisor/genesis/bin/sided keys delete $KEY --keyring-backend $KEYRING_BACKEND -y
+  /root/.side/cosmovisor/upgrades/v0.9.3/bin/sided keys delete $KEY --keyring-backend $KEYRING_BACKEND -y
 done
 
 echo "Все ключи удалены."
