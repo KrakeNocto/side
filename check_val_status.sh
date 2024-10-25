539106061f655730ab936199ecdf7c8a299bf320
KrakeNocto <octo2kraken@gmail.com>
2024-10-25 13:49:55 +0300

    Update check_val_status.sh

--- check_val_status.sh
+++ check_val_status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OUTPUT_FILE="active_vals"
+JAILED_OUTPUT_FILE="jailed_vals"
 
 VALIDATOR_ADDRESSES=(
   "cosmosvaloper1..."
@@ -9,16 +10,21 @@ VALIDATOR_ADDRESSES=(
 )
 
 > "$OUTPUT_FILE"
+> "$JAILED_OUTPUT_FILE"
 
 for VALIDATOR_ADDRESS in "${VALIDATOR_ADDRESSES[@]}"; do
-  status=$(/root/go/bin/sided query staking validator "$VALIDATOR_ADDRESS" -o json | jq -r '.status')
+  response=$(/root/go/bin/sided query staking validator "$VALIDATOR_ADDRESS" -o json)
   
-  if [[ "$status" == "BOND_STATUS_BONDED" ]]; then
+ if [[ "$status" == "BOND_STATUS_BONDED" && "$jailed" == "false" ]]; then
     echo "$VALIDATOR_ADDRESS активен"
-    echo "$VALIDATOR_ADDRESS" >> "$OUTPUT_FILE"
+    echo "$VALIDATOR_ADDRESS" >> "$ACTIVE_OUTPUT_FILE"
+  elif [[ "$jailed" == "true" ]]; then
+    echo "$VALIDATOR_ADDRESS находится в статусе jailed"
+    echo "$VALIDATOR_ADDRESS" >> "$JAILED_OUTPUT_FILE"
   else
     echo "$VALIDATOR_ADDRESS не активен"
   fi
 done
 
-echo "Список активных валидаторов записан в $OUTPUT_FILE"
+echo "Список активных валидаторов записан в $ACTIVE_OUTPUT_FILE"
+echo "Список валидаторов в статусе jailed записан в $JAILED_OUTPUT_FILE"
