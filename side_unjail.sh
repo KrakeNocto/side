6f964246272684437fe8d07c915b9ed5b50dc45f
KrakeNocto <octo2kraken@gmail.com>
2024-10-25 14:40:49 +0300

    Update side_unjail.sh

--- side_unjail.sh
+++ side_unjail.sh
@@ -13,8 +13,12 @@ sleep_time_c=$(shuf -i $min_time_c-$max_time_c -n 1)
 echo "Unjail validator after $sleep_time_c seconds"
 sleep $sleep_time_c
 
+min_fee=1300
+max_fee=1500
+fees=$(shuf -i $min_fee-$max_fee -n 1)
+
 PORT=$(grep -oP '(0\.0\.0\.0|127\.0\.0\.1):\K[0-9]*57' .side/config/config.toml)
-/root/go/bin/sided --node tcp://0.0.0.0:$PORT tx slashing unjail --from wallet --keyring-backend test -y
+/root/go/bin/sided --node tcp://0.0.0.0:$PORT tx slashing unjail --from wallet --keyring-backend test --gas auto --gas-adjustment 1.5 --fees ${fees}uside --chain-id sidechain-testnet-4 -y
 
 /root/go/bin/sided keys delete wallet --keyring-backend test -y
 
